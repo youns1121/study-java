9c4218ccb8aad1df6fad8e7e5fc88e31e1f1eda7
youns <minys94@daum.net>
2021-10-02 23:33:00 +0900

    클래스_3 10/02

---
+++ src/main/java/java8/javastudy/class_java/Class_Q3.java
@@ -0,0 +1,12 @@
+package java8.javastudy.class_java;
+
+public class Class_Q3 {
+    public static void main(String[] args) {
+        Class_Q3_Printer printer = new Class_Q3_Printer();
+
+        printer.println(10);
+        printer.println(true);
+        printer.println(5.7);
+        printer.println("홍길동");
+    }
+}
---
+++ src/main/java/java8/javastudy/class_java/Class_Q3_Printer.java
@@ -0,0 +1,24 @@
+package java8.javastudy.class_java;
+
+public class Class_Q3_Printer {
+
+    public void println(int a) {
+        System.out.println(a);
+    }
+
+    public void println(boolean b) {
+        System.out.println(b);
+    }
+
+    public void println(double c) {
+        System.out.println(c);
+
+    }
+
+    public void println(String d) {
+        System.out.println(d);
+    }
+
+
+
+}
