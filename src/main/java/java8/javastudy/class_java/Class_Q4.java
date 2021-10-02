5b1e6f8064587f482fdeb60f312ff65978c7e9b6
youns <minys94@daum.net>
2021-10-02 23:38:40 +0900

    클래스_4 10/02

---
+++ src/main/java/java8/javastudy/class_java/Class_Q4.java
@@ -0,0 +1,16 @@
+package java8.javastudy.class_java;
+
+/**
+ * 객체를 생성하지 않고 println() 메서드 사용하기
+ */
+public class Class_Q4 {
+    public static void main(String[] args) {
+
+
+
+        Class_Q4_Printer.println(10);
+        Class_Q4_Printer.println(true);
+        Class_Q4_Printer.println(5.7);
+        Class_Q4_Printer.println("홍길동");
+    }
+}
---
+++ src/main/java/java8/javastudy/class_java/Class_Q4_Printer.java
@@ -0,0 +1,28 @@
+package java8.javastudy.class_java;
+
+/**
+ * 객체를 생성하지 않고 println() 메서드 사용하기
+ * static 매서드를 활용한다
+ */
+public class Class_Q4_Printer {
+
+    public static void println(int a) {
+        System.out.println(a);
+    }
+
+    public static void println(boolean b) {
+        System.out.println(b);
+    }
+
+    public static void println(double c) {
+        System.out.println(c);
+
+    }
+
+    public static void println(String d) {
+        System.out.println(d);
+    }
+
+
+
+}
