2e386355e86c577de49acb44113638fb6d5d9115
youns <minys94@daum.net>
2021-10-04 15:00:17 +0900

    상속_1 10/04_상속 생성자 호출

--- src/main/java/java8/javastudy/inheritance/Inheritance_Q1_Child.java
+++ src/main/java/java8/javastudy/inheritance/Inheritance_Q1_Child.java
@@ -1,7 +1,10 @@
 package java8.javastudy.inheritance;
 
 import javafx.scene.Parent;
-
+/**
+ * Parent 클래스를 상속해서 Child 클래스를 다음과 같이 작성했는데, Child 클래스의 생성자에서 컴파일 에러가 발생했습니다.
+ * 그 이유를 설명해보세요 */
+//클래스 대소문자 구분
 public class Inheritance_Q1_Child extends Parent {
 
     private int studentNo;
--- src/main/java/java8/javastudy/inheritance/Inheritance_Q1_Parent.java
+++ src/main/java/java8/javastudy/inheritance/Inheritance_Q1_Parent.java
@@ -2,7 +2,8 @@ package java8.javastudy.inheritance;
 
 /**
  * Parent 클래스를 상속해서 Child 클래스를 다음과 같이 작성했는데, Child 클래스의 생성자에서 컴파일 에러가 발생했습니다.
- * 그 이유를 설명해보세요요 */
+ * 그 이유를 설명해보세요 */
+//클래스 대소문자 구분
 public class Inheritance_Q1_Parent {
 
     public String name;
@@ -11,3 +12,4 @@ public class Inheritance_Q1_Parent {
          this.name = name;
     }
 }
+
