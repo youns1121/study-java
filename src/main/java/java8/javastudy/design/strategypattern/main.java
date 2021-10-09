96035fbccaf123291865d716c1b358b7d970c4fc
youns <minys94@daum.net>
2021-10-09 19:41:11 +0900

    Strategy Pattern 10/09 전략 패턴1

---
+++ src/main/java/java8/javastudy/design/strategypattern/main.java
@@ -0,0 +1,20 @@
+package java8.javastudy.design.strategypattern;
+
+/**
+ * 전략 패턴(Strategy Pattern)
+ * 여러 알고리즘을 하나의 추삭적인 접근점(Interface)을 만들어 접근점에서 서로 교환 가능(Deligate)하도록 하는 패턴
+ */
+public class main {
+
+    public static void main(String[] args) {
+        GameCharacter character = new GameCharacter();
+        character.attack();
+
+        character.setWeapon(new Knife());
+        character.attack();
+
+        character.setWeapon(new Sword());
+        character.attack();
+
+    }
+}
