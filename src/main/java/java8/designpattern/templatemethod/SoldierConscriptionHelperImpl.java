08084f9f47521289454015d09e52b1e0185fbe19
youns <minys94@daum.net>
2021-10-11 18:56:09 +0900

    클래스 & 메소드명 변경

--- src/main/java/java8/designpattern/templatemethod/SoldierConscriptionHelperImpl.java
+++ src/main/java/java8/designpattern/templatemethod/SoldierConscriptionHelperImpl.java
@@ -1,6 +1,6 @@
 package java8.designpattern.templatemethod;
 
-public class SolderConscriptionHelperImpl extends AbstSoldierConscriptionHelper{
+public class SoldierConscriptionHelperImpl extends AbstSoldierConscriptionHelper{
     @Override
     protected Citizen conscriptionCitizen() { // 시민들 중 마린이 될 수 있는 조건의 시민 징집합니다.
         System.out.println("징집");
@@ -9,8 +9,8 @@ public class SolderConscriptionHelperImpl extends AbstSoldierConscriptionHelper{
 
     @Override
     protected void training(Citizen citizen) { //징집된 군인들을 훈련합니다.
-        citizen.updateAgility(5); //민첩성
-        citizen.updateAgility(4); //민첨성
+        citizen.updateStrength(5); // 힘
+        citizen.updateAgility(4); //민첩성
         citizen.updateIntelligence(-3); //지능
         System.out.println("병사 훈련!");
     }
