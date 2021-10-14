0bce351971b8e30763b82c7fe4db373357a139e8
youns <minys94@daum.net>
2021-10-14 23:51:27 +0900

    조건문과 반복문 21.10.14

---
+++ src/main/java/java8/javastudy/loop/ex4_9.java
@@ -0,0 +1,20 @@
+package java8.javastudy.loop;
+
+/**
+ * 숫자로 이루어진 문자열 str이 있을 때, 각 자리의 합을 더한 결과를 출력하는 코
+ * 드를 완성하라. 만일 문자열이 "12345"라면, ‘1+2+3+4+5’의 결과인 15를 출력이 출력되
+ * 어야 한다. (1)에 알맞은 코드를 넣으시오.
+ * [Hint] String클래스의 charAt(int i)을 사용
+ */
+public class ex4_9 {
+    public static void main(String[] args) {
+        String str = "12345";
+        int sum = 0;
+
+        for(int i=0; i< str.length(); i++){
+            sum+=Integer.valueOf(str.charAt(i));
+        }
+
+        System.out.println("sum = " + sum);
+    }
+}
