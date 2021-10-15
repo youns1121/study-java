8432e9aa9697f4579713b5f86058d8784da7a2eb
youns <minys94@daum.net>
2021-10-15 22:37:09 +0900

    조건문과 반복문 21.10.15

--- src/main/java/java8/javastudy/loop/ex4_13.java
+++ src/main/java/java8/javastudy/loop/ex4_13.java
@@ -7,15 +7,22 @@ package java8.javastudy.loop;
 public class ex4_13 {
     public static void main(String[] args) {
 
-        String value = "12o34";
+        String value = "12545";
         char ch = ' ';
         boolean isNumber = true;
 
         // 반복문과 charAt (int i)를 이용해서 문자열의 문자를
         // 하나씩 읽어서 검사한다.
         for(int i=0; i < value.length(); i++){
-            value.charAt(i);
+
+            if(!(value.charAt(i) >= '0' && value.charAt(i) <= '9')){
+                isNumber = false;
+
+            }
+
         }
+
+
 
         if (isNumber){
             System.out.println(value+"는 숫자입니다.");
