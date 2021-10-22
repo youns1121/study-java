2aebe7afac555eec9e5c8101f990e3f0b2a8cf6a
younsikMin <minys94@daum.net>
2021-10-22 17:54:50 +0900

    객체지향프로그래밍1

---
+++ src/main/java/java8/javastudy/objectoriented/StudaCard.java
@@ -0,0 +1,27 @@
+package java8.javastudy.objectoriented;
+
+
+//ex6_1
+public class StudaCard {
+
+    private int num;
+    private boolean isKwang;
+
+
+    public StudaCard(int num, boolean isKwang) {
+        this.num = num;
+        this.isKwang = isKwang;
+    }
+
+    public StudaCard() {
+    }
+
+//    public boolean info(){
+//
+//        return isKwang;
+//    }
+//
+//    public int info(){
+//        return num;
+//    }
+}
