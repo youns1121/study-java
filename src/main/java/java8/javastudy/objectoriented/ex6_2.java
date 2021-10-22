573e7aac6e65e78960e5f2e7419f778f26414f1b
younsikMin <minys94@daum.net>
2021-10-22 17:55:03 +0900

    객체지향프로그래밍2

---
+++ src/main/java/java8/javastudy/objectoriented/ex6_2.java
@@ -0,0 +1,10 @@
+package java8.javastudy.objectoriented;
+
+public class ex6_2 {
+    public static void main(String[] args) {
+        StudaCard card1 = new StudaCard(3, false);
+        StudaCard card2 = new StudaCard();
+
+//        System.out.println(card1.info());
+    }
+}
