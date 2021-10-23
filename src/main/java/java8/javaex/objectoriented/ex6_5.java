3ab96b7610b6d3722a159647c44c94a13344f875
youns <minys94@daum.net>
2021-10-23 18:58:04 +0900

    객체지향프로그래밍5

---
+++ src/main/java/java8/javaex/objectoriented/ex6_5.java
@@ -0,0 +1,15 @@
+package java8.javaex.objectoriented;
+
+/**
+ * 다음과 같은 실행결과를 얻도록 Student클래스에 생성자와 info()를 추가하시오.
+ */
+public class ex6_5 {
+    public static void main(String[] args) {
+
+        Student s = new Student("홍길동", 1, 1, 100, 60, 76);
+
+        System.out.println(s.info());
+
+    }
+
+}
