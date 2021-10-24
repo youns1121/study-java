ed7ad9415777ec4d3a45c95d37e1356f909aaf41
youns <minys94@daum.net>
2021-10-24 21:57:51 +0900

    객체지향프로그래밍20_미구현

---
+++ src/main/java/java8/javastudy/objectoriented/Ex6_20.java
@@ -0,0 +1,21 @@
+package java8.javastudy.objectoriented;
+
+public class Ex6_20 {
+
+    // shuffle 메서드를 작성하시오
+//    public static int shuffle(int [] original){
+//
+//        return 0;
+//    }
+
+    public static void main(String[] args) {
+
+        int [] original = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(java.util.Arrays.toString(original));
+
+//        int result = shuffle(original);
+//        System.out.println(java.util.Arrays.toString(result));
+    }
+
+
+}
