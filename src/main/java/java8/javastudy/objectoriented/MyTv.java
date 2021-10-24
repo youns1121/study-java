192d25868a599714508742ac287a4c1069e315a8
youns <minys94@daum.net>
2021-10-24 22:14:07 +0900

    객체지향프로그래밍21_미구현

---
+++ src/main/java/java8/javastudy/objectoriented/MyTv.java
@@ -0,0 +1,28 @@
+package java8.javastudy.objectoriented;
+
+//Ex6_21
+public class MyTv {
+    boolean isPowerOn;
+    int channel;
+    int volume;
+
+    final int MAX_VOLUME = 100;
+    final int MIN_VOLUME = 0;
+    final int MAX_CHANNEL = 100;
+    final int MIN_CHANNEL = 1;
+
+    void trunOnOff(){
+        // (1) isPowerOn의 값이 true면 false로, false면 true로 바꾼다.
+        if(isPowerOn == true){
+            isPowerOn = false;
+        }else{ isPowerOn = true;}
+
+//        isPowerOn == true ? false:true;
+    }
+
+    void volumUp(){
+
+    }
+
+
+}
