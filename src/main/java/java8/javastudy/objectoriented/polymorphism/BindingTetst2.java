8af73fac7a2a26f1e9aaa1309e66d5eb3c92eb9e
youns <minys94@daum.net>
2021-11-07 21:44:19 +0900

    참조변수와 인스턴스의 연결 2

---
+++ src/main/java/java8/javastudy/objectoriented/polymorphism/BindingTetst2.java
@@ -0,0 +1,27 @@
+package java8.javastudy.objectoriented.polymorphism;
+
+public class BindingTetst2 {
+    public static void main(String[] args) {
+        Parent2 p = new Child2();
+        Child2 c = new Child2();
+
+        System.out.println("p.x : "+p.x);
+        p.method();
+
+        System.out.println("c.x : "+c.x);
+        c.method();
+    }
+}
+
+class Parent2{
+    int x = 100;
+
+    void method(){
+        System.out.println("Parent Method");
+    }
+}
+
+class Child2 extends Parent2{
+
+
+}
