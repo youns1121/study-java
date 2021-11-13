f2b9a198ae9da1b0a295f828e30f52296cbd123d
youns <minys94@daum.net>
2021-11-13 16:30:39 +0900

    예외 선언하기 finally블럭 1

--- src/main/java/java8/javastudy/exception/ExceptionEx13.java
+++ src/main/java/java8/javastudy/exception/ExceptionEx13.java
@@ -3,14 +3,14 @@ package java8.javastudy.exception;
 public class ExceptionEx13 {
 
     public static void main(String[] args) {
-        method1();
+        method1(); //main 에서 예외를 발생시켜
     }
 
     static void method1(){
         try{
             throw new Exception("예외발생시키기"); //강제로 예외발생
         } catch (Exception e) {
-            System.out.println("method1메서드에서 예외가 처리되었습니다.");
+            System.out.println("method1메서드에서 예외가 처리되었습니다."); //메서드에서 예외를 처리
             e.printStackTrace();
         }
 
--- src/main/java/java8/javastudy/exception/ExceptionEx14.java
+++ src/main/java/java8/javastudy/exception/ExceptionEx14.java
@@ -5,12 +5,14 @@ public class ExceptionEx14 {
         try{
             method1();
         } catch (Exception e){
-            System.out.println("main메서드에서 예외가 처리되었습니다.");
+            System.out.println("main메서드에서 예외가 처리되었습니다."); //main에서 예외처리
             e.printStackTrace();
         }
+
     } //main 메서드 끝
 
     static void method1() throws Exception {
-        throw new Exception("ggg");
+        throw new Exception("ggg"); //메서드에서 예외를 발생시켜서
     }
+
 }
