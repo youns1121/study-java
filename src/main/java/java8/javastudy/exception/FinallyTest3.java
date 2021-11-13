ca4a55aaa7c24c895d4170ed7ce2866fb9055c57
youns <minys94@daum.net>
2021-11-13 16:42:41 +0900

    예외 선언하기 finally블럭 1

---
+++ src/main/java/java8/javastudy/exception/FinallyTest3.java
@@ -0,0 +1,26 @@
+package java8.javastudy.exception;
+
+public class FinallyTest3 {
+    public static void main(String[] args) {
+        //method1()은 static메서드이므로 인스턴스 생성없이 직접 호출이 가능하다.
+        FinallyTest3.method1();
+        System.out.println("method1()의 수행을 마치고 main메서드로 돌아왔습니다");
+    } //main메서드의 끝
+
+    private static void method1() {
+
+        try{
+            System.out.println("method1()이 호출되었습니다.");
+//            throw new Exception("예외발생");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("method1()의 finall블럭이 실행되었습니다.");
+        } //method1메서드의
+
+
+    }
+
+
+}
