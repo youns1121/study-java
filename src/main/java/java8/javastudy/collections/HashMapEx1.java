6b9df1397cfb93e392ca09b76e8261bf624cabbd
youns <minys94@daum.net>
2021-11-16 00:05:18 +0900

    HashMap 1(수정)

--- src/main/java/java8/javastudy/collections/HashMapEx1.java
+++ src/main/java/java8/javastudy/collections/HashMapEx1.java
@@ -14,17 +14,17 @@ public class HashMapEx1 {
 
         while (true) {
             System.out.println("id와 password를 입력해 주세요.");
-            System.out.println("id :");
+            System.out.print("id :");
             String id = s.nextLine().trim();
 
-            System.out.println("password :");
+            System.out.print("password :");
             String password = s.nextLine().trim();
             System.out.println();
 
             if (!map.containsKey(id)) { // HashMap에 지정된 key가 포함되어 있는지 알려준다 포함되면 true
                 System.out.println("입력하신 id는 존재하지 않습니다" + " 다시입력해 주세요.");
             } else {
-                if (map.get(id).equals(password)) {// 지정된 키(key)의 값 반환 못찾으면 null
+                if (!(map.get(id).equals(password))) {// 지정된 키(key)의 값 반환 못찾으면 null
                     System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
                 } else {
                     System.out.println("id와 비밀번호가 일치합니다.");
