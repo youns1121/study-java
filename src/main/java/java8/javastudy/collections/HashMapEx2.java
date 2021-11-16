bfbd8084d83d14057beecd018ac58be21edc5c3a
youns <minys94@daum.net>
2021-11-16 23:27:56 +0900

    HashMap 2

---
+++ src/main/java/java8/javastudy/collections/HashMapEx2.java
@@ -0,0 +1,39 @@
+package java8.javastudy.collections;
+
+import java.util.*;
+
+public class HashMapEx2 {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+
+        map.put("김자바", new Integer(90));
+        map.put("김자바", new Integer(100));
+        map.put("이자바", new Integer(100));
+        map.put("강자바", new Integer(80));
+        map.put("안자바", new Integer(90));
+
+        Set set = map.entrySet(); //HashMap에 저장된 키와 값을 엔트리의 형태로 Set에 저장해서 반환
+        Iterator it = set.iterator();
+
+        while(it.hasNext()){
+            Map.Entry e = (Map.Entry)it.next();
+            System.out.println("이름: "+e.getKey()+", 점수 : "+e.getValue());
+        }
+        set = map.keySet(); //HashMap에 저장된 모든 키가 저장된 Set을 반환
+
+        Collection values = map.values();
+        it = values.iterator();
+
+        int total = 0;
+
+        while(it.hasNext()){
+            Integer i = (Integer) it.next();
+            total += i.intValue();
+        }
+
+        System.out.println("총점 : " + total);
+        System.out.println("평균 : " + (float)total/ set.size());
+        System.out.println("최고점수 : " + Collections.max(values));
+        System.out.println("최저점수 : " + Collections.min(values));
+    }
+}
