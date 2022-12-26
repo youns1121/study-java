62a8624ff66e10b76d8513116ae67480f0da9a19
youns <minys94@daum.net>
2022-12-26 23:18:14 +0900

    모던 자바 인 액션 : 2.4.2 Runnable로 코드 블록 실행하기

--- src/main/java/java8/modern/ModernJavaInAction.java
+++ src/main/java/java8/modern/ModernJavaInAction.java
@@ -4,7 +4,14 @@ import java8.modern.chapter2.Farm;
 
 public class ModernJavaInAction {
     public static void main(String[] args) {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello World");
+            }
+        });
 
+        Thread t1 = new Thread(() -> System.out.println("Hello World"));
 
     }
 }
