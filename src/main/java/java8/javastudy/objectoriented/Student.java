37fe20c6793e34c8b73878fcf2605cb6c1801017
youns <minys94@daum.net>
2022-12-26 23:07:18 +0900

    생성자 추가

--- src/main/java/java8/javastudy/objectoriented/Student.java
+++ src/main/java/java8/javastudy/objectoriented/Student.java
@@ -19,4 +19,16 @@ public class Student {
 
         return getTotal()/3;
     }
+
+    public Student(String name, int ban, int no, int kor, int eng, int math) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+
+    public Student() {
+    }
 }
