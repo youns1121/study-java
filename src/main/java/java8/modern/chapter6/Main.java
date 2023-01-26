35e6d032848cb31297a3def148590f163437a190
youns <minys94@daum.net>
2023-01-27 00:14:33 +0900

    모던 자바 인 액션 : 6.3.2 다수준 그룹화

--- src/main/java/java8/modern/chapter6/Main.java
+++ src/main/java/java8/modern/chapter6/Main.java
@@ -137,5 +137,21 @@ public class Main {
         Map<Dish.Type, Set<String>> dishNamesBySetType = menu.stream()
                 .collect(groupingBy(Dish::getType, flatMapping(dish -> dishTags.get(dish.getName()).stream(), toSet())));
         System.out.println("dishNamesBySetType = " + dishNamesBySetType);
+
+        /**
+         * 6.3.2 다수준 그룹화
+         */
+        Map<Dish.Type, Map<CaloricLevel, List<Dish>>> dishesByTypeCaloricLevel = menu.stream().collect(
+                groupingBy(Dish::getType,
+                        groupingBy(dish -> {
+                            if (dish.getCalories() <= 400)
+                                return CaloricLevel.DIET;
+                            else if (dish.getCalories() <= 700)
+                                return CaloricLevel.NORMAL;
+                            else return CaloricLevel.FAT;
+                        })
+                )
+        );
+        System.out.println("dishesByTypeCaloricLevel = " + dishesByTypeCaloricLevel);
     }
 }
