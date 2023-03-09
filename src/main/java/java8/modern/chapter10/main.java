01e1aca9d522bc87a43d2dbe94f2ab1946bca6aa
youns <minys94@daum.net>
2023-03-09 22:10:50 +0900

    모던 자바 인 액션 : 10.2.1 스트림 API는 컬렉션을 조작하는 DSL

---
+++ src/main/java/java8/modern/chapter10/main.java
@@ -0,0 +1,59 @@
+package java8.modern.chapter10;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class main {
+    public static void main(String[] args) throws IOException {
+
+        List<String> numbers = Arrays.asList("one", "two", "three");
+        numbers.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println(s);
+            }
+        });
+
+        //내부 클래스 람다
+        numbers.forEach(s -> System.out.println(s));
+
+        // 메서드 참조
+        numbers.forEach(System.out::println);
+
+        /**
+         * 10.2.1 스트림 API는 컬렉션을 조작하는 DSL
+         */
+
+        //Before
+        String filename = "Hello.jpg";
+        List<String> errors = new ArrayList<>();
+        int errorCount = 0;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
+        String line = bufferedReader.readLine();
+        while ( line != null && errorCount < 40) {
+            if(line.startsWith("ERROR")) {
+                errors.add(line);
+                errorCount++;
+            }
+            line = bufferedReader.readLine();
+        }
+
+        //After
+
+        List<String> errorsBr = Files.lines(Paths.get(filename))
+                .filter(lineBr -> lineBr.startsWith("ERROR"))
+                .limit(40)
+                .collect(Collectors.toList());
+
+
+    }
+}
