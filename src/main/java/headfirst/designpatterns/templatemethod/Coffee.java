4139543c6e9ba24f1edfb1900179587b097dbff3
youns <minys94@daum.net>
2023-05-18 00:09:26 +0900

    헤드 퍼스트 디자인패턴 : CHAPTER 8. 템플릿 메소드 패턴 (추상화)

--- src/main/java/headfirst/designpatterns/templatemethod/Coffee.java
+++ src/main/java/headfirst/designpatterns/templatemethod/Coffee.java
@@ -1,27 +1,14 @@
 package headfirst.designpatterns.templatemethod;
 
-public class Coffee {
+public class Coffee extends CaffeineBeverage{
 
-    void prepareRecipe() {
-        boilWater();
-        brewCoffeeGrinds();
-        pourInCup();
-        addSugarAndMilk();
-    }
-
-    public void boilWater() {
-        System.out.println("물 끓이는 중");
-    }
-
-    public void brewCoffeeGrinds() {
+    @Override
+    void brew() {
         System.out.println("필터로 커피를 우려내는 중");
     }
 
-    public void pourInCup() {
-        System.out.println("컵에 따르는 중");
-    }
-
-    public void addSugarAndMilk() {
+    @Override
+    void addCondiments() {
         System.out.println("설탕과 우유를 추가하는 중");
     }
 }
--- src/main/java/headfirst/designpatterns/templatemethod/Tea.java
+++ src/main/java/headfirst/designpatterns/templatemethod/Tea.java
@@ -1,27 +1,14 @@
 package headfirst.designpatterns.templatemethod;
 
-public class Tea {
+public class Tea extends CaffeineBeverage{
 
-    void prepareRecipe() {
-        boilWater();
-        steepTeaBag();
-        pourInCup();
-        addLemon();
-    }
-
-    public void boilWater() {
-        System.out.println("물 끓이는 중");
-    }
-
-    public void steepTeaBag() {
+    @Override
+    void brew() {
         System.out.println("찻잎을 우려내는 중");
     }
 
-    public void addLemon() {
+    @Override
+    void addCondiments() {
         System.out.println("레몬을 추가하는 중");
-    }
-
-    public void pourInCup() {
-        System.out.println("컵에 따르는 중");
     }
 }
