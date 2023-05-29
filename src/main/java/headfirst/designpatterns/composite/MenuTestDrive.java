39aff4175ca6e3cda076c0a33cac52b480519505
youns <minys94@daum.net>
2023-05-29 23:30:20 +0900

    헤드 퍼스트 디자인패턴 : CHAPTER 9. 반복자 패턴과 컴포지트 패턴 (종업원 코드 테스트)

---
+++ src/main/java/headfirst/designpatterns/composite/MenuTestDrive.java
@@ -0,0 +1,11 @@
+package headfirst.designpatterns.composite;
+
+public class MenuTestDrive {
+    public static void main(String[] args) {
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinerMenu dinerMenu = new DinerMenu();
+
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+        waitress.printMenu();
+    }
+}
