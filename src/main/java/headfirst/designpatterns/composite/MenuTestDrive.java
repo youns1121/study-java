13876f2eb60fb794dbc593e58035f5e932d30e77
youns <minys94@daum.net>
2023-06-04 00:22:18 +0900

    헤드 퍼스트 디자인패턴 : CHAPTER 9. 반복자 패턴과 컴포지트 패턴 (카페 메뉴 추가)

--- src/main/java/headfirst/designpatterns/composite/MenuTestDrive.java
+++ src/main/java/headfirst/designpatterns/composite/MenuTestDrive.java
@@ -4,8 +4,9 @@ public class MenuTestDrive {
     public static void main(String[] args) {
         Menu pancakeHouseMenu = new PancakeHouseMenu();
         Menu dinerMenu = new DinerMenu();
+        Menu cafeMenu = new CafeMenu();
 
-        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
         waitress.printMenu();
     }
 }
--- src/main/java/headfirst/designpatterns/composite/Waitress.java
+++ src/main/java/headfirst/designpatterns/composite/Waitress.java
@@ -6,18 +6,23 @@ public class Waitress {
 
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+        Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
 
         System.out.println("메뉴\n----\n아침 메뉴");
         printMenu(pancakeIterator);
+        System.out.println("\n 점심메뉴");
+        printMenu(cafeIterator);
         System.out.println("\n저녁 메뉴");
         printMenu(dinerIterator);
     }
@@ -30,7 +35,6 @@ public class Waitress {
                 System.out.print(menuItem.getPrice() + " -- ");
                 System.out.println(menuItem.getDescription());
             }
-
         }
     }
 }
